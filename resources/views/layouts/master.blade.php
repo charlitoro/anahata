ab281d2ba496e85d2d32086ab64b14fe336c1ca2
Andres Toro <carlostoro04@gmail.com>
2021-02-01 20:00:48 -0500

    configuracion de la pagina pricipal en base la plantilla

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,19 +10,19 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ url('/assets/anahata/lib/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('/assets/anahata/lib/bootstrap/css/bootstrap.min.css')}}">
     
 
     <link href="{{url('/assets/anahata/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{ url('/assets/anahata/lib/animate/animate.min.css')}}" rel="stylesheet">
-    <link href="{{ url('/assets/anahata/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
-    <link href="{{ url('/assets/anahata/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-    <link href="{{ url('/assets/anahata/lib/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
-    <link href="{{ url('/assets/anahata/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+    <link href="{{url('/assets/anahata/lib/animate/animate.min.css')}}" rel="stylesheet">
+    <link href="{{url('/assets/anahata/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+    <link href="{{url('/assets/anahata/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{url('/assets/anahata/lib/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{url('/assets/anahata/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ url('/assets/anahata/css/style.css')}}">
+    <link rel="stylesheet" href="{{url('/assets/anahata/css/style.css')}}">
 
-    <title>Hello, world</title>
+    <title>Estetica Anahata</title>
   </head>
   <body id="body">
 
@@ -31,23 +31,23 @@
 
       <div id="logo" class="pull-left">
           <h1><a href="index.html" id="body" class="scrollto"><span><i class="ion 	
-            ion-android-notifications"></i> </span>brando</a></h1> 
+            ion-android-notifications"></i> </span>Anahata</a></h1> 
         <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
       </div>
-        @include('partials.nav-bar')
-        <div class="container">
-          @yield('content')
-        </div>
+
+      @include('partials.nav-bar')
+
     </div>
   </header>
+
+  @yield('content')
+
   @include('partials.footer')
-
-
 
     <!-- JavaScript  -->
     <script src="{{url('/assets/anahata/lib/jquery/jquery.min.js')}}"></script>
     <script src="{{url('/assets/anahata/lib/jquery/jquery-migrate.min.js')}}"></script>
-    <script src="{{ url('/assets/anahata/lib/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{url('/assets/anahata/lib/bootstrap/js/bootstrap.min.js')}}"></script>
     <script src="{{url('/assets/anahata/lib/easing/easing.min.js')}}"></script>
     <script src="{{url('/assets/anahata/lib/superfish/hoverIntent.js')}}"></script>
     <script src="{{url('/assets/anahata/lib/superfish/superfish.min.js')}}"></script>
@@ -56,9 +56,7 @@
     <script src="{{url('/assets/anahata/lib/magnific-popup/magnific-popup.min.js')}}"></script>
     <script src="{{url('/assets/anahata/lib/sticky/sticky.js')}}"></script> 
     
-    <script src="{{ url('/assets/anahata/js/main.js') }}"></script>
-
-    
+    <script src="{{url('/assets/anahata/js/main.js')}}"></script>
     
   </body>
 </html>
